d1ceacf24575a176de1418fee52688393522ce94
EduRibeiro00 <eribeiro306@gmail.com>
2019-10-20 16:13:39 +0100

    Ja agr faco isto

--- Trabalho_1/noncanonical.c
+++ Trabalho_1/noncanonical.c
@@ -27,38 +27,6 @@ int main(int argc, char** argv)
       exit(1);
     }
 
-    // // fills appLayer fields
-    // al.status = RECEIVER;
-
-    // if ((al.fileDescriptor = llopen(argv[1], al.status)) <= 0)
-    // {
-    //     return -1;
-    // }
-
-    // unsigned char dataBuffer[20];
-    // unsigned char packetBuffer[MAX_DATA_SIZE];
-    // int packetLength;
-    // int sequenceNumber;
-    // int dataLength;
-
-    // if((packetLength = llread(al.fileDescriptor, packetBuffer)) < 0)
-    //   return -1;
-
-    // for(int i = 0; i < packetLength; i++) {
-    //   printf("%x\n", packetBuffer[i]);
-    // }
-
-    // if(parseDataPacket(packetBuffer, dataBuffer, &sequenceNumber) < 0)
-    //   return -1;
-
-    // printf("Packet length: %d\n", packetLength);
-    // printf("Sequence number: %d\n", sequenceNumber);
-    // printf("Data length: %d", packetLength - 4);
-
-    // for(int i = 0; i < packetLength - 4; i++) {
-    //   printf("%x\n", dataBuffer[i]);
-    // }
-
     if(receiveFile(argv[1])<0){
       return -1;
     }
