940085e07d1de8ccf555379dc8ac2db9bcf3bbb1
diogo <diogo.smac@gmail.com>
2019-11-10 17:57:39 +0000

    Code beautification + Doxygen documentation

--- Trabalho_2/src/main.c
+++ Trabalho_2/src/main.c
@@ -3,13 +3,15 @@
 #include "functions.h"
 
 int main(int argc, char *argv[]) {
-    if(argc != 2) {
-        printf("Usage: ./%s %s", argv[0], "ftp://[<user>:<password>@]<host>/<url-path>");
+
+    if (argc != 2) {
+        printf("Usage: ./%s %s", argv[0], "ftp:// [<user>:<password>@]<host>/<url-path>");
         return -1;
     }
+
     // parse command line arguments
     struct arguments args;
-    if(parseArguments(&args, argv[1]) != 0) {
+    if (parseArguments(&args, argv[1]) != 0) {
         return -1;
     }
     printf("User: %s\n", args.user);
@@ -20,23 +22,24 @@ int main(int argc, char *argv[]) {
 
     struct ftp ftp;
 
-    char command[MAX_LENGTH]; // buffer to send commands
-    char responseBuffer[MAX_LENGTH]; // buffer to read commands
+    char command[MAX_LENGTH];           /* buffer to send commands */
+    char responseBuffer[MAX_LENGTH];    /* buffer to read commands */
 
-    // get IP Adress
-    char ipAdress[MAX_LENGTH];
-    if(getIPAdress(ipAdress, args.host_name) < 0) {
+    // get IP Address
+    char ipAddress[MAX_LENGTH];
+    if (getIPAddress(ipAddress, args.host_name) < 0) {
         return -1;
     }
+
     // create and connect socket to server
-    if((ftp.control_socket_fd = createAndConnectSocket(ipAdress, FTP_PORT_NUMBER)) < 0) {
+    if ((ftp.control_socket_fd = createAndConnectSocket(ipAddress, FTP_PORT_NUMBER)) < 0) {
         printf("Error creating new socket\n");
         return -1;
     }
     
     receiveFromControlSocket(&ftp, responseBuffer, MAX_LENGTH);
     
-    if(responseBuffer[0] == '2') {
+    if (responseBuffer[0] == '2') {
         printf("Expecting username...\n\n");
     }
     else{
@@ -44,23 +47,20 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    if(login(&ftp, args.user, args.password)<0){
+    if (login(&ftp, args.user, args.password)<0){
         printf("Login failed...\n\n");
         return -1;
     }
 
-    if(changeWorkingDirectory(&ftp, args.file_path)<0){
+    if (changeWorkingDirectory(&ftp, args.file_path)<0){
         printf("Error changing directory\n");
         return -1;
     }
 
-    if(getServerPortForFile(&ftp)<0){
+    if (getServerPortForFile(&ftp)<0){
         printf("Error getting server Port for file\n");
         return -1;
     }
-
-
-
 
     return 0;
 }
